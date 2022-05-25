447c0ba3a8383ab6bf0e857ade859485f4f5f3a2
suzukiyoshiki <yoshisu04183@gmail.com>
2022-05-25 14:47:00 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
 
 const App = () => {
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 20 },
-    { name: "Nanami", age: 1  }
-  ]
   return (
     <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
+      <Counter></Counter>
     </div>
   )
 }
 
-const User = (props) => {
-  return (
-    <div>こんにちは！{ props.name }です！{ props.age }歳です！</div>
-  )
-}
+class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0}
+  }
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
+  handlePlusButton = () => {
+    console.log("handlePlusButton")
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  handleMinusButton = () => {
+    console.log("handleMinusButton")
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: { this.state.count }</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;

30ad7a73d96087002b81f4665318e3bc54e90beb
suzukiyoshiki <yoshisu04183@gmail.com>
2022-05-27 13:42:41 +0900

    events-index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,10 @@
+import axios from "axios"
+export const READ_EVENTS = "READ_EVENTS"
 
-export const INCREMENT = "INCREMENT"
-export const DECREMENT = "DECREMENT"
+const ROOT_URL = "https://udemy-utils.herokuapp.com/api/v1"
+const QUERYSTRING = "?token=token123"
 
-export const increment = () => ({
-  type: DECREMENT
-})
-
-export const decrement = () => ({
-  type: DECREMENT
-})
+export const readEvents = () => async dispatch =>  {
+  const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
+  dispatch({ type: READ_EVENTS, response })
+}
--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "@reduxjs/toolkit";
+// import { applyMiddleware } from "redux";
+// import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
 import './index.css';
 import reducer from './reducers';
-import App from './components/App';
+import EventsIndex from './components/events_index';
 import reportWebVitals from './reportWebVitals';
 // import registerServiceWorker from "./registerServiceWorker";
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
-  </Provider>
-  // document.getElementById("root")
+    <EventsIndex />
+  </Provider>,
+  document.getElementById("root")
 );
 // registerServiceWorker();
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
+  // <Provider store={store}>
+  //   <EventsIndex />
+  // </Provider>
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>
